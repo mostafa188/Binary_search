ca09f3128519cdbad3149e86ef2a43d54d75dc14
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2022-08-31 16:10:27 +0600

    bs

---
+++ last_occerence.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solve(int ara[], int n,int item){
+    int l,r,mid,answer;
+
+    l = 0 ;
+    r = n -1;
+    while(l<=r){
+        mid = (l+r) / 2 ;
+        if(ara[mid] == item){
+            answer = mid ;
+            l = mid+1 ;
+        }
+        if(ara[mid] < item ) l = mid+1 ;
+        else r = mid-1;
+    }
+
+    return answer;
+}
+int main(){
+    int n,answer,x;
+    cin >> n>>x;   
+
+    int ara[n+1];
+    for(int i = 0; i< n; i ++){
+        cin >> ara[i];
+    }
+
+    answer = solve(ara,n,x);
+    cout<<answer<<'\n';
+    return 0;
+}
